f5d408b4503a474aa2e63510c0a3623b87c987f1
StartaBafras <beyhankaradag00@gmail.com>
2023-02-26 12:07:36 +0300

    stack eklendi

--- chip8.h
+++ chip8.h
@@ -4,7 +4,9 @@
 #define REGISTERS 16
 #define SCREEN_X 64
 #define SCREEN_Y 32
-#define PIXEL_SHIFTING 150
+#define PIXEL_SHIFTING 75
+#define ZOOM_RATE 10
+#define STACK_SIZE 10
 
 typedef struct chip8
 {
@@ -17,6 +19,16 @@ typedef struct chip8
 
 }chip8;
 
+typedef struct stack
+{
+    short int stack[STACK_SIZE];
+    short int stack_level;
+    short int (*pop) (struct stack *stack_value);
+    short int (*push) (struct stack *stack_value, short int value);
+    
+}stack;
+
+
 chip8* chip8_init();
 
 int fetch(chip8 *emulator);
@@ -24,6 +36,10 @@ int fetch(chip8 *emulator);
 void decode_execute(chip8 *emulator);
 
 int read_rom(chip8 *emulator);
+
+short int pop(stack *stack_value);
+
+short int push(stack *stack_value, short int value);
 
 
 
