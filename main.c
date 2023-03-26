889684037383b4357ebda56e6846de7df3ed52b7
StartaBafras <beyhankaradag00@gmail.com>
2023-03-26 10:34:35 +0300

    Gecikmeler düzenlendi ancak fps limiti bozuk

--- main.c
+++ main.c
@@ -1,28 +1,26 @@
 #include "gfx.h"
 #include "chip8.h"
-#include<time.h>
+#include <time.h>
 #include <unistd.h>
+#include <sys/time.h>
+#include <stdio.h>
+
+
+
 
 
 int main()
 {
-    
-	int ysize = 600;
-	int xsize = 600;
-    clock_t start, stop;
+    struct timeval start, end;
+
+	int ysize = 500;
+	int xsize = 800;
 
 
-	// Open a new window for drawing.
+
+
 	gfx_open(xsize,ysize,"Chip-8");
 
-	// Set the current drawing color to green.
-	gfx_color(0,200,100);
-    //gfx_point(0,0);
-    //gfx_point(300,300);
-    //gfx_point(150,150);
-    //gfx_flush();
-    
-	// Draw a triangle on the screen.
 
     chip8 *emulator = chip8_init();
 
@@ -30,13 +28,30 @@ int main()
 
     for(;;)
     {
-        start = clock();
+        
+        gettimeofday(&start, NULL);
         fetch(emulator);
         decode_execute(emulator);
         gfx_flush();
-        stop = clock();
-        sleep( (16-(start-stop))/1000); //62.5 Hz
+        msleep(1);
+        //msleep((16,666666667-(micros*1000))); //62.5 Hz
+        gettimeofday(&end, NULL);
 
+        long seconds = (end.tv_sec - start.tv_sec);
+        long micros = ((seconds * 1000000) + end.tv_usec) - (start.tv_usec);
+        
+        printf(" %d seconds %d micros\n", seconds, micros);
+
+        
+        if(emulator->delay_timer > 0)
+        {
+            emulator->delay_timer--;
+        }
+        if(emulator->sound_timer > 0)
+        {
+            printf("\a");
+            emulator->sound_timer--;
+        }
     }
 
 	return 0;
